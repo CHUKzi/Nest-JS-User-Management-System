0cc11966764d96fb4e90ed5a09d18e0d9823c355
royanharsha <royan.g@parallaxtec.com>
2024-12-14 03:06:58 +0530

    Add : Users CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { ValidationPipe } from "@nestjs/common";
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
+        synchronize: true,
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({length: 50 })
+    firstName: string;
+  
+    @Column({length: 50 })
+    lastName: string;
+  
+    @Column({length: 75 })
+    email: string;
+
+    @Column({length: 100 })
+    address: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    registerDate: Date;
+}
